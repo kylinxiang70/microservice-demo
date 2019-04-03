015aee833e5d305008c0d3c0ed8a1ef93e9f7c90
kylinxiang <15884286060@163.com>
2019-04-03 09:21:06 +0800

    add serialVersionUID for exception

--- auth-center/src/main/java/org/kylin/authcenter/exception/UserOperationException.java
+++ auth-center/src/main/java/org/kylin/authcenter/exception/UserOperationException.java
@@ -1,6 +1,8 @@
 package org.kylin.authcenter.exception;
 
 public class UserOperationException extends RuntimeException {
+    private static final long serialVersionUID = 8468616518092020748L;
+
     public UserOperationException(String msg) {
         super(msg);
     }
