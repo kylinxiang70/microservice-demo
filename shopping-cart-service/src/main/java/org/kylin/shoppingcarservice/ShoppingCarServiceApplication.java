7df7c67252ca55de51b50d8a2b2e5e83e8b0c58d
kylinxiang <15884286060@163.com>
2019-04-10 20:25:26 +0800

    rename shopping-car-service to shopping-cart-service

---
+++ shopping-cart-service/src/main/java/org/kylin/shoppingcarservice/ShoppingCarServiceApplication.java
@@ -0,0 +1,13 @@
+package org.kylin.shoppingcarservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCarServiceApplication.class, args);
+	}
+
+}
